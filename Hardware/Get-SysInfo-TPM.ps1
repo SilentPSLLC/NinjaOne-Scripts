cf543596a5026fec418d25501ddbb1eed5faa7da
SilentPSLLC <143986845+SilentPSLLC@users.noreply.github.com>
2025-02-28 15:09:38 -0600

    Update Get-SysInfo-TPM.ps1
    Rewrote code

--- Hardware/Get-SysInfo-TPM.ps1
+++ Hardware/Get-SysInfo-TPM.ps1
@@ -1,64 +1,64 @@
+<#
+.SYNOPSIS
+  Scrapes system information from managed endpoints.
+
+.DESCRIPTION
+  This script retrieves the endpoint's First Physical Disk name.
+
+.NOTES
+  Company: SilentPS, LLC
+  URL: https://silentps.com 
+  Created On: 21 January, 2024
+  Modified: 28 Febuarary, 2025
+  Version: 1.0
+  License: All Rights Reserved
+
+.LINK
+  https://github.com/SilentPSLLC/NinjaOne-Scripts/
+     
+.EXAMPLE
+  ./Get-SysInfo-DiskName.ps1
+#>    
+
 # Define TPM Version Mapping (GUID -> Human Readable)
-$TPMVersionMap = @{
-    "2c12064a-8fa8-4856-be3b-8ec7d4afd827" = "Unknown"
-    "e1986642-d42e-4593-bcc7-51ddb079a61a" = "Not Present"
-    "1b49c0de-faba-4265-8b38-3e845313665c" = "1.0"
-    "f52af764-ebd0-4e32-b1ce-b599ab36a20b" = "1.5"
-    "7022bd76-b3e9-431f-b04f-3464187b9c2b" = "2.0"
-    "106fc692-17fc-4164-aca7-1e192a511d67" = "2.5"
-}
+  $TPMVersionMap = @{
+    "17f564d8-e842-497d-8e0b-5e856cd0af2e" = "2.5"
+    "a3910e2b-57b8-47ae-9caf-6768b97ffc0c" = "2.0"
+    "14752995-6e05-4a47-b758-0e120b6adb45" = "1.0"
+    "5a0c94f0-1a8c-48ee-97e1-80d900511cad" = "1.5"
+    "65ba5650-df06-4021-aafa-b4720dd7b6b1" = "Not Present"
+    "1a379091-81c3-4074-a5fd-8c2b30a1590c" = "Unknown"
+  }
 
-# Define TPM Compatibility Mapping (GUID -> Human Readable)
-$TPMCompatibilityMap = @{
-    "0cbd061a-8687-4b8b-b0a9-517f45b978ad" = "False"
-    "44f8f66d-add4-4e72-9b50-98848fa90371" = "Unknown"
-    "c20ae679-d2fd-4f90-8614-5f58b413b81d" = "True"
-}
+  # Fetch TPM Information
+  $GetTPM = Get-WmiObject -Namespace "Root\CIMv2\Security\MicrosoftTpm" -Class Win32_Tpm
+  $GetOldTPMVersion = Ninja-Property-Get tpmVersion
 
-# Fetch TPM Information
-$GetTPM = Get-WmiObject -Namespace "Root\CIMv2\Security\MicrosoftTpm" -Class Win32_Tpm
+  # Default to "Not Present"
+  $TPMVersionGUID = "e1986642-d42e-4593-bcc7-51ddb079a61a"  # Not Present
 
-# Default to "Not Present"
-$TPMVersionGUID = "e1986642-d42e-4593-bcc7-51ddb079a61a"  # Not Present
-$TPMCompatibilityGUID = "0cbd061a-8687-4b8b-b0a9-517f45b978ad"  # False
-
-# Check if TPM exists
-if ($GetTPM) {
+  # Check if TPM exists
+  if ($GetTPM) {
     $SpecVersion = $GetTPM.SpecVersion
     Write-Host "Detected TPM Spec Version: $SpecVersion"
 
     switch -Wildcard ($SpecVersion) {
-        "*Not Present*" { $TPMVersionGUID = "e1986642-d42e-4593-bcc7-51ddb079a61a" }
-        "*1.0*" { $TPMVersionGUID = "1b49c0de-faba-4265-8b38-3e845313665c" }
-        "*1.5*" { $TPMVersionGUID = "f52af764-ebd0-4e32-b1ce-b599ab36a20b" }
-        "*2.0*" { $TPMVersionGUID = "7022bd76-b3e9-431f-b04f-3464187b9c2b" }
-        "*2.5*" { $TPMVersionGUID = "106fc692-17fc-4164-aca7-1e192a511d67" }
-        Default { $TPMVersionGUID = "2c12064a-8fa8-4856-be3b-8ec7d4afd827" } # Unknown
+        "*Not Present*" { $TPMVersionGUID = "65ba5650-df06-4021-aafa-b4720dd7b6b1" } # Not Present
+        "*1.0*" { $TPMVersionGUID = "14752995-6e05-4a47-b758-0e120b6adb45" } # 1.0
+        "*1.5*" { $TPMVersionGUID = "5a0c94f0-1a8c-48ee-97e1-80d900511cad" } # 1.5
+        "*2.0*" { $TPMVersionGUID = "a3910e2b-57b8-47ae-9caf-6768b97ffc0c" } # 2.0 Required miniumums for Windows 11
+        "*2.5*" { $TPMVersionGUID = "17f564d8-e842-497d-8e0b-5e856cd0af2e" } # 2.5
+        Default { $TPMVersionGUID = "1a379091-81c3-4074-a5fd-8c2b30a1590c" } # Unknown
     }
+  } else {Write-Host "No TPM detected."}
 
-    # Determine TPM Compatibility
-    if ($TPMVersionGUID -eq "7022bd76-b3e9-431f-b04f-3464187b9c2b" -or $TPMVersionGUID -eq "106fc692-17fc-4164-aca7-1e192a511d67") {
-        $TPMCompatibilityGUID = "c20ae679-d2fd-4f90-8614-5f58b413b81d"  # True (Compatible)
-    } elseif ($TPMVersionGUID -eq "e1986642-d42e-4593-bcc7-51ddb079a61a") {
-        $TPMCompatibilityGUID = "0cbd061a-8687-4b8b-b0a9-517f45b978ad"  # False (Not Present)
-    } else {
-        $TPMCompatibilityGUID = "44f8f66d-add4-4e72-9b50-98848fa90371"  # Unknown
-    }
-} else {
-    Write-Host "No TPM detected."
-}
-
-# Debugging Property Set
-Write-Host "Setting TPM Version GUID to: $TPMVersionGUID"
-Write-Host "Setting TPM Compatibility GUID to: $TPMCompatibilityGUID"
-
-# Send GUIDs to NinjaOne
+# Update & Return TPM Version information
 Ninja-Property-Set tpmVersion $TPMVersionGUID
-Ninja-Property-Set CompatibleTPM $TPMCompatibilityGUID
-
-# Final Debugging Confirmation
-Write-Host "Successfully updated NinjaOne properties."
-
-# Display Human-Readable Output
-Write-Host "TPM Version: $($TPMVersionMap[$TPMVersionGUID])"
-Write-Host "Windows 11 Compatible TPM: $($TPMCompatibilityMap[$TPMCompatibilityGUID])"
+Write-Host "Updated TPM Version from $GetOldTPMVersion to: $($TPMVersionMap[$TPMVersionGUID]) ($TPMVersionGUID)"
+Write-Host "GUID List: "
+Write-Host "17f564d8-e842-497d-8e0b-5e856cd0af2e (2.5)"
+Write-Host "a3910e2b-57b8-47ae-9caf-6768b97ffc0c (2.0)"
+Write-Host "14752995-6e05-4a47-b758-0e120b6adb45 (1.0)"
+Write-Host "5a0c94f0-1a8c-48ee-97e1-80d900511cad (1.5)"
+Write-Host "65ba5650-df06-4021-aafa-b4720dd7b6b1 (Not Present)"
+Write-Host "1a379091-81c3-4074-a5fd-8c2b30a1590c (Unknown)"
