d30697469f51f900c724f2011419afe0d1160dc7
SilentPSLLC <143986845+SilentPSLLC@users.noreply.github.com>
2025-02-28 14:13:48 -0600

    Update Documentation & Remove White Spaces

--- Hardware/Get-SysInfo-Processor.ps1
+++ Hardware/Get-SysInfo-Processor.ps1
@@ -1,25 +1,24 @@
 <#
 .SYNOPSIS
-	Scrapes system information from managed endpoints.
+  Scrapes system information from managed endpoints.
 
 .DESCRIPTION
-	This script retrieves the endpoint's Processor Name.
+  This script retrieves the endpoint's Processor Name.
 
 .NOTES
-	Company: SilentPS, LLC
-	URL: https://silentps.com 
-	Created On: 21 January, 2024
-	Modified: 28 Febuarary, 2025
-	Version: 1.0
-	License: All Rights Reserved
+  Company: SilentPS, LLC
+  URL: https://silentps.com 
+  Created On: 21 January, 2024
+  Modified: 28 Febuarary, 2025
+  Version: 1.0
+  License: All Rights Reserved
 
 .LINK
-	https://github.com/SilentPSLLC/NinjaOne-Scripts/
+  https://github.com/SilentPSLLC/NinjaOne-Scripts/
      
 .EXAMPLE
-	./Get-SysInfo-Processor.ps1
+  ./Get-SysInfo-Processor.ps1
 #>    
-
 
 # Get the name of the installed processor
 $Processor = Get-WmiObject -Class Win32_Processor | Select-Object -ExpandProperty Name
@@ -28,4 +27,3 @@ $OldProcessor = Ninja-Property-Get Processor
 # Update and Return $Processor
 Ninja-Property-Set Processor $Processor
 Write-Host "Updated Processor from $OldProcessor to $Processor Successfully"
-
