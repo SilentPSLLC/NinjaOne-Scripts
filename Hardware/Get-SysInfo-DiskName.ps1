57ae9631b32428d7b0d87b942708a7d914acee9a
SilentPSLLC <143986845+SilentPSLLC@users.noreply.github.com>
2025-02-28 14:14:43 -0600

    Moved to Hardware/Get-SysInfo-DiskName.ps1 & Basic Houskeeping

--- Hardware/Get-SysInfo-DiskName.ps1
+++ Hardware/Get-SysInfo-DiskName.ps1
@@ -1,25 +1,24 @@
 <#
 .SYNOPSIS
-	Scrapes system information from managed endpoints.
+  Scrapes system information from managed endpoints.
 
 .DESCRIPTION
-	This script retrieves the endpoint's First Physical Disk name.
+  This script retrieves the endpoint's First Physical Disk name.
 
 .NOTES
-	Company: SilentPS, LLC
-	URL: https://silentps.com 
-	Created On: 21 January, 2024
-	Modified: 28 Febuarary, 2025
-	Version: 1.0
-	License: All Rights Reserved
+  Company: SilentPS, LLC
+  URL: https://silentps.com 
+  Created On: 21 January, 2024
+  Modified: 28 Febuarary, 2025
+  Version: 1.0
+  License: All Rights Reserved
 
 .LINK
-	https://github.com/SilentPSLLC/NinjaOne-Scripts/
+  https://github.com/SilentPSLLC/NinjaOne-Scripts/
      
 .EXAMPLE
-	./Get-SysInfo-DiskName.ps1
+  ./Get-SysInfo-DiskName.ps1
 #>    
-
 
 # Get the name of the installed processor
 $DiskName = (Get-PhysicalDisk | Select-Object -First 1 FriendlyName).FriendlyName
