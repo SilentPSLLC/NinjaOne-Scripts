3129d6299b2f7ad16a26bc23446d0893f1931fe8
SilentPSLLC <143986845+SilentPSLLC@users.noreply.github.com>
2025-02-28 15:45:19 -0600

    Update Get-SysInfo-DiskHealth.ps1 for clean up white spaces

--- Hardware/Get-SysInfo-DiskHealth.ps1
+++ Hardware/Get-SysInfo-DiskHealth.ps1
@@ -3,21 +3,21 @@
   Scrapes system information from managed endpoints.
 
 .DESCRIPTION
-	This script retrieves the endpoint's First Physical Disk Health Status.
+  This script retrieves the endpoint's First Physical Disk Health Status.
 
 .NOTES
-	Company: SilentPS, LLC
-	URL: https://silentps.com 
-	Created On: 21 January, 2024
-	Modified: 28 Febuarary, 2025
-	Version: 1.0
-	License: All Rights Reserved
+  Company: SilentPS, LLC
+  URL: https://silentps.com 
+  Created On: 21 January, 2024
+  Modified: 28 Febuarary, 2025
+  Version: 1.0
+  License: All Rights Reserved
 
 .LINK
-	https://github.com/SilentPSLLC/NinjaOne-Scripts/
+  https://github.com/SilentPSLLC/NinjaOne-Scripts/
      
 .EXAMPLE
-	./Get-SysInfo-DiskHealth.ps1
+  ./Get-SysInfo-DiskHealth.ps1
 #>    
 
 # Get the name of the installed processor
